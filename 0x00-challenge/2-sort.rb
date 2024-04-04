1aa27ed6c7f3484edb33e4875ab339c9f528bbe1
yassineboucham <yassineboucham11@gamil.com>
2024-04-04 15:47:34 +0100

    h

--- 0x00-challenge/2-sort.rb
+++ 0x00-challenge/2-sort.rb
@@ -13,13 +13,15 @@ ARGV.each do |arg|
     i_arg = arg.to_i
     
     # insert result at the right position
+    is_inserted = false
     i = 0
     l = result.size
     while !is_inserted && i < l do
-        if result[i] < i_arg
+        if result[i] > i_arg
             i += 1
         else
             result.insert(i - 1, i_arg)
+            is_inserted = true
             break
         end
     end
